74a86b4f1a43717ef2ceedf13bca3612e8a88f6b
MarioOrlando82 <mario796gt@gmail.com>
2024-10-05 14:20:23 +0700

    fix: environment variables

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
+import firebaseApp from "./firebase";
+import { getFirestore } from "firebase/firestore";
+
+const db = getFirestore(firebaseApp);
 
 ReactDOM.render(
   <React.StrictMode>
