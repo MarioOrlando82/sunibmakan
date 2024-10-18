79ffcf8f661f3ae6dedf6da1edb60ba4747ffef8
Thevid Oswald Berrius <thevidob@gmail.com>
2024-10-18 17:13:27 +0700

    Color Header Roullete

--- src/components/RoulettePage.tsx
+++ src/components/RoulettePage.tsx
@@ -41,7 +41,7 @@ const RoulettePage: React.FC = () => {
 
   return (
     <div className="container mx-auto mt-8">
-      <h2 className="text-2xl font-bold mb-4">Roulette Review</h2>
+      <h2 className="text-2xl font-bold mb-4  text-pastel-dark">Roulette Review</h2>
       <button
         onClick={handleRoulette}
         className="bg-pastel-primary text-white px-4 py-2 rounded-lg mb-4 hover:bg-pastel-accent transition-colors"
