d7bb032bc97970675dd0433861804f861608e2bd
Thevid Oswald Berrius <thevidob@gmail.com>
2024-10-27 10:22:30 +0700

    Style Leaderboard

--- src/components/Leaderboard.tsx
+++ src/components/Leaderboard.tsx
@@ -35,22 +35,33 @@ const Leaderboard: React.FC = () => {
   };
 
   return (
-    <div className="container mx-auto mt-8">
-      <h2 className="text-2xl font-bold mb-4">Leaderboard</h2>
-      <table className="min-w-full bg-white">
+    <div className="container mx-auto mt-8 bg-pastel-light rounded-lg shadow-lg p-3">
+      <h2 className="text-3xl font-bold mb-4 text-center pt-3 text-pastel-dark">Leaderboard</h2>
+      <table className="min-w-full border-collapse">
         <thead>
           <tr>
-            <th className="py-2">Rank</th>
-            <th className="py-2">User</th>
-            <th className="py-2">Reviews</th>
+            <th className="py-2 bg-pastel-lightDark text-pastel-dark">Rank</th>
+            <th className="py-2 bg-pastel-lightDark text-pastel-dark">User</th>
+            <th className="py-2 bg-pastel-lightDark text-pastel-dark">Reviews</th>
           </tr>
         </thead>
         <tbody>
           {leaderboard.map((user, index) => (
-            <tr key={user.userId} className="border-t">
-              <td className="py-2 px-4 text-center">{index + 1}</td>
-              <td className="py-2 px-4">{user.username}</td>
-              <td className="py-2 px-4 text-center">{user.reviewCount}</td>
+            <tr
+              key={user.userId}
+              className={`${
+                index === 0
+                  ? "bg-pastel-gold text-pastel-dark font-bold"
+                  : index === 1
+                  ? "bg-pastel-silver text-pastel-dark font-semibold"
+                  : index === 2
+                  ? "bg-pastel-bronze text-pastel-dark font-semibold"
+                  : "bg-white text-pastel-dark"
+              } border-b border-pastel-lightDark`}
+            >
+              <td className="py-4 px-6 text-center text-xl">{index + 1}</td>
+              <td className="py-4 px-6 text-left">{user.username}</td>
+              <td className="py-4 px-6 text-center text-lg">{user.reviewCount}</td>
             </tr>
           ))}
         </tbody>
--- tailwind.config.js
+++ tailwind.config.js
@@ -14,6 +14,9 @@ module.exports = {
           dark: '#6b705c',
           primary: '#cb997e',
           accent: '#ffb4a2',
+          gold: '#FFD966',
+          silver: '#D9D9D9',
+          bronze: '#E8A87C',
         },
       },
       fontFamily: {
